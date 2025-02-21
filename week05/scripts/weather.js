48b0fb2fefccc296d8bedf97cd51065376939cf7
angie <angiewheeler99@gmail.com>
2025-02-20 18:26:50 -0700

    changes

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,9 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=c5c949e9cb0fdc6349341c62c7907378"
+//const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=c5c949e9cb0fdc6349341c62c7907378"
+
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=49.75&lon=6.64&exclude=hourly,daily&appid=c5c949e9cb0fdc6349341c62c7907378"
 
 //Define an asynchronous function named "apiFetch()" that uses a try block to handle errors.
 async function apiFetch() {
@@ -21,9 +23,9 @@ async function apiFetch() {
     }
 }
 
-function displayReults(data) {
+function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&units=imperial;F`;
-    const iconsrc = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png';
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute("src", iconsrc);
     weatherIcon.setAttribute("alt", desc);
