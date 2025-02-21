bb63e223eef63f5a191b060fc94c4b6bd0776c42
angie <angiewheeler99@gmail.com>
2025-02-20 19:33:22 -0700

    json file changes to membership

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -48,3 +48,69 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 });
 
+//Company spotlights members.json file
+
+[
+    {
+        "name": "Burly Burger",
+        "address": "2572 E South Weber Dr, South Weber, Utah 84405",
+        "phone": "801-479-7459",
+        "url": "https://burlyburger.com/",
+        "image": "images/burlyburger.webp",
+        "membership": "silver",
+        "info": "GET SOME BURGER IN YOUR BEARD! We make burgers fresh, tasty and bigger than most people can eat..."
+    },
+    {
+        "name": "Sodalicious",
+        "address": "2532 S South Weber drive, South Weber, Utah 84405",
+        "phone": "385-835-2864",
+        "url": "https://mysodalicious.com",
+        "image": "images/sodalicious.webp",
+        "membership": "bronze",
+        "info": "MIXOLOGY: Let us help you find your perfect drink."
+    },
+    {
+        "name": "Little Caesars",
+        "address": "2572 E South Weber Dr Ste 1, South Weber, UT 84405",
+        "phone": "801-528-7749",
+        "url": "https://littlecaesars.com/en-us/store/11704/",
+        "image": "images/littlecaesers.webp",
+        "membership": "bronze",
+        "info": "Hot N Ready pizza to go"
+    },
+    {
+        "name": "Maverick",
+        "address": "2577 S Weber Dr, South Weber, UT 84405",
+        "phone": "801-479-7918",
+        "url": "https://locations.maverik.com/ut/south-weber/2577-south-weber-drive",
+        "image": "images/maverik.webp",
+        "membership": "gold",
+        "info": "Adventure's First Stop"
+    },
+    {
+        "name": "Morty's Car Wash",
+        "address": "7723 S 2700 E, South Weber, Utah 84405",
+        "phone": "385-432-0761",
+        "url": "https://mortyscarwash.com/contact/",
+        "image": "images/mortys.webp",
+        "membership": "bronze",
+        "info": "Convenient, state-of-the-art car wash in South Weber, Utah."
+    },
+    {
+        "name": "ski 'N see",
+        "address": "2532 South Weber Drive, South Weber, Utah 84405",
+        "phone": "801-462-0960",
+        "url": "https://www.skinsee.com/locations/south-weber/",
+        "image": "images/ski.webp",
+        "membership": "gold",
+        "info": "Utah's Local Ski & Board Rental Shop"
+    },
+    {
+        "name": "Mountain Land Physical Therapy",
+        "address": "2572 South Weber Drive Ste. 5, South Weber, Utah 84405",
+        "phone": "801-479-7045",
+        "url": "https://www.mlpt.com/southweber/",
+        "image": "images/mountain.webp",
+        "membership": "gold",
+        "info": "Physical Therapy"
+    }
